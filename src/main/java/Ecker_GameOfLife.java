53b332a180d721f048875a0039c27826797ae2e6
QuirinEcker <quirin.ecker@bajupa.com>
2018-05-08 09:09:36 +0200

    initial project

---
+++ src/main/java/Ecker_GameOfLife.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class Ecker_GameOfLife {
+
+    public static void main(String[] args) {
+        PApplet.main("MainClass", args);
+    }
+
+    public void settings() {
+
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+}
